562347695cbc59a4bdbe0fc2ce2cf64aba1df1a0
rabuu <rasbuur@gmail.com>
2021-03-29 15:10:55 +0200

    Make short net script standard

--- config.def.h
+++ config.def.h
@@ -2,7 +2,8 @@ static const Block blocks[] = {
 	/* icon */   /* command */                   /* update interval */   /* update signal */
 	{ "",         "dwmblocks_hostanduser",			0,						0 },
 	{ "",         "dwmblocks_keyboard_layout",		5,						1 },
-	{ "",         "dwmblocks_network",				5,						0 },
+	/* { "",         "dwmblocks_network",				5,						0 }, */
+	{ "",         "dwmblocks_network_short",		5,						0 },
 	{ "",         "dwmblocks_volume",				0,						2 },
 	{ "",         "dwmblocks_memory",				5,						0 },
 //	{ "",         "dwmblocks_swap",					5,						0 },
