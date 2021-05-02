5f6de53965a436a4a2f5a966ddb208db02362857
rabuu <rabuu.mail@gmail.com>
2021-05-02 22:28:10 +0200

    Sync everything always

--- config.def.h
+++ config.def.h
@@ -1,11 +1,11 @@
 static const Block blocks[] = {
 	/* icon */   /* command */                   /* update interval */   /* update signal */
 	{ "",         "dwmblocks_hostanduser",			0,						0 },
-	{ "",         "dwmblocks_keyboard_layout",		5,						1 },
+	{ "",         "dwmblocks_keyboard_layout",		1,						1 },
 	/* { "",         "dwmblocks_network",				5,						0 }, */
-	{ "",         "dwmblocks_network_short",		5,						0 },
-	{ "",         "dwmblocks_volume",				0,						2 },
-	{ "",         "dwmblocks_memory",				5,						0 },
+	{ "",         "dwmblocks_network_short",		1,						0 },
+	{ "",         "dwmblocks_volume",				1,						2 },
+	{ "",         "dwmblocks_memory",				1,						0 },
 //	{ "",         "dwmblocks_swap",					5,						0 },
 	{ "",         "dwmblocks_date",					1,						0 },
 //	{ "",         "dwmblocks_battery",				5,						0 },
